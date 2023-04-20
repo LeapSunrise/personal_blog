cba36eb506f757d818aa020a1f9e3d433bc5e529
LeapSunrise <rostyslav.melnyk49@gmail.com>
2023-04-19 18:03:27 -0600

    login page create

--- personal_blog/main_page/urls.py
+++ personal_blog/main_page/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='main_page')
+    path('', views.base, name='main_page'),
+    path('login', views.login, name='login')
 ]
--- personal_blog/main_page/views.py
+++ personal_blog/main_page/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, redirect
 
 def base(request):
     return render(request, 'main_page/base.html')
+
+
+def login(request):
+    return render(request, 'main_page/page_login.html')
